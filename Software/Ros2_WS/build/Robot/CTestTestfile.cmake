aa483230d9275cfa6cde2bd9a7c3c540067c07a7
SuperDuu <vuducdu2k5@gmail.com>
2026-01-17 22:28:08 +0700

    add ros2_pkg

---
+++ Software/Ros2_WS/build/Robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/du/Desktop/NCKH_2026/Software/Ros2_WS/urdf
+# Build directory: /home/du/Desktop/NCKH_2026/Software/Ros2_WS/build/Robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

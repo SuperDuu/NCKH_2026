737cc22f74a0cb20912bd55e721db1ba980dcc7e
dat <dat30581@gmail.com>
2026-01-19 16:48:29 +0700

    RViz launch file

---
+++ Software/Ros2_WS/build/ros2_pkg/CMakeFiles/uvc_node.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dat/Robot/NCKH_2026/Software/Ros2_WS/src/ros2_pkg/src/uvc_node.cpp" "CMakeFiles/uvc_node.dir/src/uvc_node.cpp.o" "gcc" "CMakeFiles/uvc_node.dir/src/uvc_node.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Software/Ros2_WS/build/ros2_pkg/CMakeFiles/uvc_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uvc_node.dir/src/uvc_node.cpp.o"
+  "CMakeFiles/uvc_node.dir/src/uvc_node.cpp.o.d"
+  "uvc_node"
+  "uvc_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uvc_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
